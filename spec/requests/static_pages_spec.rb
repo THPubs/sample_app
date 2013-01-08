d29b61c0f385b1b48b80d52e41bd4c940e292632
Pubudu Kodikara <pubudu.th@gmail.com>
2013-01-08 10:40:53 +0530

    Finish 5.6 Exercises

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,14 +3,19 @@ require 'spec_helper'
 	describe "Static pages" do
 
 		subject {page}
+
+		shared_examples_for "all static pages" do
+			it { should have_selector('h1', text: heading) }
+			it { should have_selector('title', text: full_title(page_title)) }
+		end
 		
 		describe "Home page" do
 
 			before {visit root_path}
 
-			it { should have_selector('h1', text: 'Sample App') }
-
-			it { should have_selector('title', text: full_title('') ) }
+			let(:heading) { 'Sample App' }
+			let(:page_title) { '' }
+			it_should_behave_like "all static pages"
 
 			it { should_not have_selector('title', text: full_title('Home')) }
 
@@ -19,9 +24,9 @@ require 'spec_helper'
 		describe "Help page" do
 			before {visit help_path}
 
-			it { should have_selector('h1', text: 'Help') }
-
-			it { should have_selector('title', text: full_title('Help')) }
+			let(:heading) { 'Help' }
+			let(:page_title) { 'Help' }
+			it_should_behave_like "all static pages"
 
 		end
 
@@ -29,9 +34,9 @@ require 'spec_helper'
 
 			before {visit about_path}
 
-			it {should have_selector('h1', text: 'About Us')}
-
-			it {should have_selector('title', text: full_title('About Us'))}
+			let(:heading) { 'About Us' }
+			let(:page_title) { 'About Us' }
+			it_should_behave_like "all static pages"
 
 		end
 
@@ -39,9 +44,33 @@ require 'spec_helper'
 
 			before {visit contact_path}
 
-			it { should have_selector('h1', text: 'Contact') }
+			let(:heading) { 'Contact' }
+			let(:page_title) { 'Contact' }
+			it_should_behave_like "all static pages"
 
-			it { should have_selector('title', text: full_title('Contact')) }
+		end
+
+		it "should have the right links on the layouts" do
+
+			visit root_path
+
+			click_link "About"
+			page.should have_selector 'title', text: full_title('About Us')
+
+			click_link "Help"
+			page.should have_selector 'title', text: full_title( 'Help' )
+
+			click_link "Contact"
+			page.should have_selector 'title', text: full_title( 'Contact' )
+
+			click_link "Home"
+			page.should have_selector 'title', text: full_title( '' )
+
+			click_link "Sign up now!"
+			page.should have_selector 'title', text: full_title( 'Sign up' )
+
+			click_link "sample app"
+			page.should have_selector 'title', text: full_title( '' )
 
 		end
 
